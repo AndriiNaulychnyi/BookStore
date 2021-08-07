35948fd3d2dca4c4f2cd120cc4e88307136ec665
AndriiNaulychnyi <andriy.naulychnyi@gmail.com>
2021-08-07 19:50:41 +0300

    Add BasketController

--- src/main/java/com/example/bookstore/controller/BasketController.java
+++ src/main/java/com/example/bookstore/controller/BasketController.java
@@ -1,9 +1,19 @@
 package com.example.bookstore.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.bookstore.models.Book;
+import com.example.bookstore.service.BasketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BasketController {
 
+    @Autowired
+    private BasketService basketService;
 
+    @GetMapping("/api/basket?userId={userId}&bookId={bookId}")
+    public Book addBookToBasket(@RequestParam Long userId,
+                                @RequestParam Long bookId) {
+        return basketService.addBookToBasket(userId, bookId);
+    }
 }
