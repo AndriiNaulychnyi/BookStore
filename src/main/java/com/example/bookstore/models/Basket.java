30d94e9486de5c6d29e1f2a5114618b783080d41
AndriiNaulychnyi <andriy.naulychnyi@gmail.com>
2021-08-07 19:48:49 +0300

    Add setAllPrice method

--- src/main/java/com/example/bookstore/models/Basket.java
+++ src/main/java/com/example/bookstore/models/Basket.java
@@ -8,7 +8,12 @@ import java.util.List;
 @Data
 public class Basket {
 
+    private Long id;
     private final List<Book> basket = new ArrayList<>();
     private int allPrice;
+
+    public void setAllPrice(int price) {
+        this.allPrice += price;
+    }
 
 }
