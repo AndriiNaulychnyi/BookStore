659e976612b32d154d04c19779c6dfb154895396
AndriiNaulychnyi <andriy.naulychnyi@gmail.com>
2021-08-07 15:48:16 +0300

    Add two fields

--- src/main/java/com/example/bookstore/models/Basket.java
+++ src/main/java/com/example/bookstore/models/Basket.java
@@ -2,7 +2,13 @@ package com.example.bookstore.models;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class Basket {
-    
+
+    private final List<Book> basket = new ArrayList<>();
+    private int allPrice;
+
 }
