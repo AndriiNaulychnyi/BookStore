ccad7639d1d5f27fda18ade7a7ccf92243a65521
AndriiNaulychnyi <andriy.naulychnyi@gmail.com>
2021-08-20 18:16:46 +0300

    add getBasket and clearBasket methods and updated other methods

--- src/main/java/com/example/bookstore/service/BasketService.java
+++ src/main/java/com/example/bookstore/service/BasketService.java
@@ -12,44 +12,26 @@ import org.springframework.stereotype.Service;
 public class BasketService {
 
     private final BasketRepository basketRepository;
-    private final UserService userService;
     private final BookService bookService;
-
-    // TODO: why this var is dramatically bad approach?
-    double discount;
 
     public Basket newBasket() {
         return basketRepository.newBasket();
     }
 
-    public Book addBookToBasket(Long userId, Long bookId) {
-        if (bookId % 3 == 0) {
-            discount = 0.1;
-        }
-        Long basketId = userService.getUser(userId).getBasket().getId();
+    public Book addBookToBasket(Long basketId, Long bookId) {
         Book book = bookService.getBook(bookId);
         return basketRepository.addBookToBasket(basketId, book);
     }
 
-    public void createOrder(Long userId) {
-        Basket basket = userService.getUser(userId).getBasket();
-        double resultPrice = basket.getTotalPrice() * discount;
+    public Basket removeFromBasket(Long basketId, Long bookId) {
+        return basketRepository.removeFromBasket(basketId, bookId);
     }
 
-    public Basket removeFromBasket(Long id, Long bookId) {
-        Basket basket = userService.getUser(id).getBasket();
-        return basketRepository.removeFromBasket(basket.getId(), bookId);
+    public Basket getBasket(Long basketId) {
+        return basketRepository.getBasket(basketId);
     }
 
-    public void getTotalPrice(Basket basket) {
-        if (basket.getProduct().isEmpty()) {
-            basket.setTotalPrice(0);
-            return;
-        }
-        int basketTotalPrice = 0;
-        for (Book book: basket.getProduct().values()) {
-            basketTotalPrice += book.getPrice();
-        }
-        basket.setTotalPrice(basketTotalPrice);
+    public Basket clearBasket(Long basketId) {
+        return basketRepository.clearBasket(basketId);
     }
 }
