fffed7d71feda8c93fbe2330605db56b705b7a8a
AndriiNaulychnyi <andriy.naulychnyi@gmail.com>
2021-08-21 11:33:20 +0300

    add UserRepositoryInterface.java

---
+++ src/main/java/com/example/bookstore/repository/UserRepositoryInterface.java
@@ -0,0 +1,14 @@
+package com.example.bookstore.repository;
+
+import com.example.bookstore.models.User;
+
+import java.util.List;
+
+public interface UserRepositoryInterface {
+    List<User> getAll();
+    User getUser(Long id);
+    User addUser(User user);
+    User updateUser(Long id, User user);
+    User deleteUser(Long id);
+
+}
